6f19f3bb26336698c7a82bc9bfdd1b4ce3546205
Lorenzo91347 <lorenzo.losito91@gmail.com>
2023-10-31 16:07:45 +0100

    feat:input shown

--- myscript.js
+++ myscript.js
@@ -1,4 +1,17 @@
 'use strict'
-const elementName = document.getElementById('name')
-const elemenSurname = document.getElementById('surname')
-const elementColor = document.getElementById('color')
+const elementName = document.getElementById('name');
+const elementSurname = document.getElementById('surname');
+const elementColor = document.getElementById('color');
+const number = Math.floor((Math.random() * 10) + 1);
+
+
+const nameInput = prompt('insert your name');
+const surnameInput = prompt('insert your surname');
+const colorInput = prompt('insert your favourite color');
+
+elementName.innerHTML = nameInput + surnameInput +colorInput + number; 
+
+console.log(elementName);
+ 
+
+
